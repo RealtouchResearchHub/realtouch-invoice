afec219e685c942546882ee7600604c4e4b55216
Realtouch Global Ventures <rgvlimited@gmail.com>
2025-12-22 02:11:06 +0000

    auto-commit for 4f8f9341-83e4-4870-a2b8-671ec26cd0ab

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -112,6 +112,7 @@ function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<LoginPage />} />
       <Route 
         path="/dashboard/*" 
         element={
